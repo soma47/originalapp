420180a1771a0531b935894257cad7073ba94893
Soma <soma4747@gmail.com>
2015-05-02 22:33:10 +0900

    宿題だったので一応...。まだ何もできてないです

--- original app/ViewController.swift
+++ original app/ViewController.swift
@@ -5,20 +5,74 @@
 //  Created by Soma on 2015/04/27.
 //  Copyright (c) 2015年 Soma. All rights reserved.
 //
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    // MapView.
+    var myMapView : MKMapView!
+    var myLocationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // MapViewの生成.
+        myMapView = MKMapView()
+        
+        // MapViewのサイズを画面全体に.
+        myMapView.frame = self.view.bounds
+        
+        // Delegateを設定.
+        myMapView.delegate = self
+        
+        // MapViewをViewに追加.
+        self.view.addSubview(myMapView)
+        
+        // 中心点の緯度経度.
+        let myLat: CLLocationDegrees = 37.506804
+        let myLon: CLLocationDegrees = 139.930531
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon)
+        
+        // 縮尺.
+        let myLatDist : CLLocationDistance = 100
+        let myLonDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist);
+        
+        // MapViewに反映.
+        myMapView.setRegion(myRegion, animated: true)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    // Regionが変更された時に呼び出されるメソッド.
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        println("regionDidChangeAnimated")
+        
+        // MapViewを生成.
+        var mapView : MKMapView = MKMapView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
+        
+        // MapViewの中心位置を指定.
+        mapView.centerCoordinate = CLLocationCoordinate2DMake(37.331741, -122.030333)
+        
+        // 縮尺を変更.
+        // 倍率を指定.
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        // MapViewで指定した中心位置とMKCoordinateSapnで宣言したspanを指定する.
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: span)
+        
+        // MapViewのregionプロパティにregionを指定.
+        mapView.region = region
+        
+        // MapViewをviewに追加.
+        self.view.addSubview(mapView)
+        
+      
+    
+}
 
 
 }
